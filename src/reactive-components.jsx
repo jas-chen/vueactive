7bdf8b9258fbd39ca5d8caae3e4936910bbbe8b7
Jas Chen <jas@shoplineapp.com>
2020-09-02 15:03:55 +0800

    only R

--- src/reactive-components.jsx
+++ src/reactive-components.jsx
@@ -1,32 +1,45 @@
-import React, { useMemo, useState, useEffect, useRef } from 'react';
+import { useMemo, useState, useEffect, useRef } from 'react';
 import { effect, stop, unref } from '@vue/reactivity';
 
-export const Reactive = ({ children: render }) => {
+const scheduler = (() => {
+  let jobs = new Set();
+  let isFlushing = false;
+  const executeJobs = () => {
+    for (const job of jobs) {
+      job();
+    }
+
+    jobs = new Set();
+    isFlushing = false;
+  }
+
+  return (job) => {
+    jobs.add(job);
+    if (!isFlushing) {
+      isFlushing = true;
+      Promise.resolve().then(executeJobs);
+    }
+  }
+})();
+
+const R = ({ children, ...props }) => {
+  const render = useMemo(() => {
+    return typeof children === 'function'
+      ? children
+      : () => unref(children);
+  }, [children]);
   const [element, setElement] = useState(render);
   const effectRef = useRef();
   useEffect(() => {
-    effectRef.current = effect(() => {
+    const update = () => {
       if (!effectRef.current) {
         render()
       } else {
         setElement(render());
       }
-    },{
-      scheduler: (() => {
-        let callback;
-        let isFlushing = false;
-        return (job) => {
-          callback = job;
-          if (!isFlushing) {
-            isFlushing = true;
-            requestAnimationFrame(() => {
-              callback();
-              isFlushing = false;
-            });
-          }
-        }
-      })()
-    });
+    }
+
+    effectRef.current = effect(update, { scheduler, ...props });
 
     return () => {
       stop(effectRef.current);
@@ -36,13 +49,4 @@ export const Reactive = ({ children: render }) => {
   return element;
 }
 
-export const reactive = (render) =>
-  <Reactive>{
-    typeof render === 'function'
-      ? render
-      : () => unref(render)
-  }</Reactive>
-
-export const useMemoOnce = (fn) => {
-  return useMemo(fn, []);
-}
+export default R;
