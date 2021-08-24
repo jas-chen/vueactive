84a86df7d957d6c8f9816500077e4a9462c9047b
Jas Chen <jas@shoplineapp.com>
2021-08-24 22:51:13 +0800

    upgrade to @vue/reactivity ^3.2.4

--- examples/TodoMVC/index.js
+++ examples/TodoMVC/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { ref, reactive, computed, unref as _ } from "@vue/reactivity";
-import { useRunOnce, Watch } from "vueactive";
+import { reactive as $, component } from "./vueactive";
+
+const { Input, Ul, Li, Label, A } = component;
 
 document.head.insertAdjacentHTML(
   "beforeend",
@@ -45,245 +47,218 @@ const filterKeyLabelMap = {
 };
 
 const NewTodoInput = ({ onSubmit }) => {
-  return useRunOnce(() => {
-    const label$ = ref("");
+  const label$ = useState(() => ref(""))[0];
 
-    return (
-      <Watch sync>
-        {() => (
-          <input
-            className="new-todo"
-            placeholder="What needs to be done?"
-            value={_(label$)}
-            onChange={(e) => {
-              label$.value = e.target.value;
-            }}
-            onKeyPress={(e) => {
-              if (e.key === "Enter" && e.target.value) {
-                onSubmit(e.target.value.trim());
-                label$.value = "";
-              }
-            }}
-          />
-        )}
-      </Watch>
-    );
-  });
+  return (
+    <Input
+      ref={(element) => element?.focus()}
+      className="new-todo"
+      placeholder="What needs to be done?"
+      value={_(label$)}
+      onChange={(e) => {
+        label$.value = e.target.value;
+      }}
+      onKeyPress={(e) => {
+        if (e.key === "Enter" && e.target.value) {
+          onSubmit(e.target.value.trim());
+          label$.value = "";
+        }
+      }}
+      props={() => ({
+        value: _(label$),
+      })}
+    />
+  );
 };
 
 const EditTodoInput = ({ initLabel, onSubmit, onFinish }) => {
-  return useRunOnce(() => {
-    const label$ = ref(initLabel);
+  const label$ = useState(() => ref(initLabel))[0];
 
-    return (
-      <Watch sync>
-        {() => (
-          <input
-            className="edit"
-            value={_(label$)}
-            ref={(input) => input && input.focus()}
-            onChange={(e) => {
-              label$.value = e.target.value;
-            }}
-            onBlur={onFinish}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                onSubmit(e.target.value);
-                onFinish();
-              } else if (e.key === "Escape") {
-                label$.value = "";
-                onFinish();
-              }
-            }}
-          />
-        )}
-      </Watch>
-    );
-  });
+  return (
+    <Input
+      className="edit"
+      ref={(input) => input && input.focus()}
+      onChange={(e) => {
+        label$.value = e.target.value;
+      }}
+      onBlur={onFinish}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          onSubmit(e.target.value);
+          onFinish();
+        } else if (e.key === "Escape") {
+          label$.value = "";
+          onFinish();
+        }
+      }}
+      props={() => ({
+        value: _(label$),
+      })}
+    />
+  );
 };
 
 const App = ({ routeName$ = Router.routeName$ }) => {
-  return useRunOnce(() => {
-    const todoList = reactive([]);
-    const editingTodoId$ = ref();
+  const todoList = useState(reactive([]))[0];
+  const editingTodoId$ = useState(() => ref())[0];
 
-    const filteredTodoList$ = computed(() => {
-      const routeName = _(routeName$);
-      if (routeName === "ALL") {
-        return todoList;
-      }
+  const filteredTodoList$ = computed(() => {
+    const routeName = _(routeName$);
+    if (routeName === "ALL") {
+      return todoList;
+    }
 
-      const filterValue = routeName === "COMPLETED";
-      return todoList.filter(({ done }) => done === filterValue);
-    });
-
-    const itemsLeft$ = computed(() =>
-      todoList.reduce((sum, todo) => (sum += !todo.done ? 1 : 0), 0)
-    );
-
-    const isAllCompleted$ = computed(
-      () =>
-        todoList.length &&
-        todoList.reduce((sum, todo) => sum + (todo.done ? 1 : 0), 0) ===
-          todoList.length
-    );
-
-    const onToggleAll = () => {
-      const done = isAllCompleted$.value ? false : true;
-      todoList.forEach((todo) => {
-        todo.done = done;
-      });
-    };
-
-    const onClearCompletedClick = () => {
-      const notDoneTodoList = todoList.filter(({ done }) => !done);
-      todoList.length = notDoneTodoList.length;
-      notDoneTodoList.forEach((todo, i) => {
-        todoList[i] = todo;
-      });
-    };
-
-    return (
-      <>
-        <div className="todoapp">
-          <header className="header">
-            <h1>todos</h1>
-            <NewTodoInput
-              onSubmit={(label) =>
-                todoList.unshift({
-                  id: new Date().getTime(),
-                  label,
-                  done: false,
-                })
-              }
-            />
-          </header>
-          <section className="main">
-            <Watch>
-              {() => (
-                <input
-                  id="toggle-all"
-                  type="checkbox"
-                  className="toggle-all"
-                  checked={_(isAllCompleted$)}
-                  onChange={onToggleAll}
-                />
-              )}
-            </Watch>
-            <label htmlFor="toggle-all" />
-            <Watch>
-              {() => (
-                <ul className="todo-list">
-                  {_(filteredTodoList$).map((todo) => (
-                    <Watch key={todo.id}>
-                      {() => (
-                        <li
-                          className={[
-                            todo.done ? "completed" : "",
-                            _(editingTodoId$) === todo.id ? "editing " : "",
-                          ].join(" ")}
-                        >
-                          <div className="view">
-                            <Watch>
-                              {() => (
-                                <input
-                                  type="checkbox"
-                                  className="toggle"
-                                  checked={todo.done}
-                                  onChange={() => {
-                                    todo.done = !todo.done;
-                                  }}
-                                />
-                              )}
-                            </Watch>
-                            <Watch>
-                              {() => (
-                                <label
-                                  onDoubleClick={() => {
-                                    editingTodoId$.value = todo.id;
-                                  }}
-                                >
-                                  {todo.label}
-                                </label>
-                              )}
-                            </Watch>
-                            <button
-                              className="destroy"
-                              onClick={() => {
-                                todoList.splice(
-                                  todoList.findIndex(
-                                    ({ id }) => id === todo.id
-                                  ),
-                                  1
-                                );
-                              }}
-                            />
-                          </div>
-                          <Watch>
-                            {() =>
-                              _(editingTodoId$) === todo.id && (
-                                <EditTodoInput
-                                  initLabel={todo.label}
-                                  onSubmit={(label) => {
-                                    todo.label = label;
-                                  }}
-                                  onFinish={() => {
-                                    editingTodoId$.value = null;
-                                  }}
-                                />
-                              )
-                            }
-                          </Watch>
-                        </li>
-                      )}
-                    </Watch>
-                  ))}
-                </ul>
-              )}
-            </Watch>
-          </section>
-          <footer className="footer">
-            <span className="todo-count">
-              <strong>
-                <Watch>{itemsLeft$}</Watch>
-              </strong>{" "}
-              items left
-            </span>
-            <ul className="filters">
-              {["ALL", "ACTIVE", "COMPLETED"].map((filterKey) => (
-                <li key={filterKey}>
-                  <Watch>
-                    {() => (
-                      <a
-                        href={Router.getPath(filterKey)}
-                        className={
-                          _(routeName$) === filterKey ? "selected" : ""
-                        }
-                      >
-                        {filterKeyLabelMap[filterKey]}
-                      </a>
-                    )}
-                  </Watch>
-                </li>
-              ))}
-            </ul>
-            <button className="clear-completed" onClick={onClearCompletedClick}>
-              Clear completed
-            </button>
-          </footer>
-        </div>
-        <footer className="info">
-          <p>Double-click to edit a todo</p>
-          <p>
-            Created by <a href="http://github.com/jas-chen/">Jas Chen</a>
-          </p>
-          <p>
-            Part of <a href="http://todomvc.com">TodoMVC</a>
-          </p>
-        </footer>
-      </>
-    );
+    const filterValue = routeName === "COMPLETED";
+    return todoList.filter(({ done }) => done === filterValue);
   });
+
+  const itemsLeft$ = computed(() =>
+    todoList.reduce((sum, todo) => (sum += !todo.done ? 1 : 0), 0)
+  );
+
+  const isAllCompleted$ = computed(
+    () =>
+      todoList.length &&
+      todoList.reduce((sum, todo) => sum + (todo.done ? 1 : 0), 0) ===
+        todoList.length
+  );
+
+  const onToggleAll = () => {
+    const done = isAllCompleted$.value ? false : true;
+    todoList.forEach((todo) => {
+      todo.done = done;
+    });
+  };
+
+  const onClearCompletedClick = () => {
+    const notDoneTodoList = todoList.filter(({ done }) => !done);
+    todoList.length = notDoneTodoList.length;
+    notDoneTodoList.forEach((todo, i) => {
+      todoList[i] = todo;
+    });
+  };
+
+  return (
+    <>
+      <div className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <NewTodoInput
+            onSubmit={(label) =>
+              todoList.unshift({
+                id: new Date().getTime(),
+                label,
+                done: false,
+              })
+            }
+          />
+        </header>
+        <section className="main">
+          <Input
+            id="toggle-all"
+            type="checkbox"
+            className="toggle-all"
+            onChange={onToggleAll}
+            props={() => ({
+              checked: _(isAllCompleted$),
+            })}
+          />
+          <label htmlFor="toggle-all" />
+          <Ul className="todo-list">
+            {() =>
+              _(filteredTodoList$).map((todo) => (
+                <Li
+                  key={todo.id}
+                  props={() => ({
+                    className: [
+                      todo.done ? "completed" : "",
+                      _(editingTodoId$) === todo.id ? "editing " : "",
+                    ].join(" "),
+                  })}
+                >
+                  <div className="view">
+                    <Input
+                      type="checkbox"
+                      className="toggle"
+                      onChange={() => {
+                        todo.done = !todo.done;
+                      }}
+                      props={() => ({
+                        checked: todo.done,
+                      })}
+                    />
+                    <Label
+                      onDoubleClick={() => {
+                        editingTodoId$.value = todo.id;
+                      }}
+                    >
+                      {() => todo.label}
+                    </Label>
+                    <button
+                      className="destroy"
+                      onClick={() => {
+                        todoList.splice(
+                          todoList.findIndex(({ id }) => id === todo.id),
+                          1
+                        );
+                      }}
+                    />
+                  </div>
+                  {$(() =>
+                    _(editingTodoId$) === todo.id && (
+                      <EditTodoInput
+                        initLabel={todo.label}
+                        onSubmit={(label) => {
+                          todo.label = label;
+                        }}
+                        onFinish={() => {
+                          editingTodoId$.value = null;
+                        }}
+                      />
+                    )
+                  )}
+                </Li>
+              ))
+            }
+          </Ul>
+        </section>
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>
+              {$(() => _(itemsLeft$))}
+            </strong>{" "}
+            items left
+          </span>
+          <ul className="filters">
+            {["ALL", "ACTIVE", "COMPLETED"].map((filterKey) => (
+              <li key={filterKey}>
+                <A
+                  href={Router.getPath(filterKey)}
+                  props={() => ({
+                    className: _(routeName$) === filterKey ? "selected" : "",
+                  })}
+                >
+                  {filterKeyLabelMap[filterKey]}
+                </A>
+              </li>
+            ))}
+          </ul>
+          <button className="clear-completed" onClick={onClearCompletedClick}>
+            Clear completed
+          </button>
+        </footer>
+      </div>
+      <footer className="info">
+        <p>Double-click to edit a todo</p>
+        <p>
+          Created by <a href="http://github.com/jas-chen/">Jas Chen</a>
+        </p>
+        <p>
+          Part of <a href="http://todomvc.com">TodoMVC</a>
+        </p>
+      </footer>
+    </>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,20 @@ import {
   useState,
   useEffect,
   useRef,
-  createElement,
   useCallback,
   useMemo,
+  memo,
+  createElement,
+  Component,
+  forwardRef,
+  Fragment,
 } from "react";
 import {
   effect as reactivityEffect,
   stop,
-  ref,
-  reactive,
-  readonly,
   unref,
-  pauseTracking,
-  resetTracking,
-  isRef,
 } from "@vue/reactivity";
 
-const emptyObject = {};
 const dumbEffect = (callback) => callback();
 
 let effect;
@@ -32,198 +29,131 @@ const isBrowser = typeof window !== "undefined" && globalThis === window;
 
 setIsStaticRendering(!isBrowser);
 
-const nextTickScheduler = (() => {
-  let jobs = new Set();
-  let isFlushing = false;
-  const executeJobs = () => {
-    for (const job of jobs) {
-      job();
-    }
+// const useWatchEffect = (fn, options) => {
+//   const effectRef = useRef(useState(() => {
+//     return effect(fn, options);
+//   })[0]);
 
-    jobs = new Set();
-    isFlushing = false;
-  };
+//   useEffect(() => {
+//     if (!effectRef.current) {
+//       effectRef.current = effect(fn, options);
+//     }
 
-  return (job) => {
-    jobs.add(job);
-    if (!isFlushing) {
-      isFlushing = true;
-      Promise.resolve().then(executeJobs);
-    }
-  };
-})();
+//     return () => {
+//       console.log('unmount')
+//       stop(effectRef.current);
+//       effectRef.current = null;
+//     };
+//   }, []);
+// };
 
-const refModel = (initValue) => {
-  const value$ = ref(initValue);
-  const setValue = (newValue) => {
-    value$.value = newValue;
-  };
-  return [readonly(value$), setValue];
-};
+// const useWatchValue = (ref, options) => {
+//   const done = useRef();
+//   const firstValue = useRef();
 
-const reactiveModel = (actions, initialArg, init) => {
-  const value$ = reactive(init ? init(initialArg) : initialArg);
-  const finalActions = {};
-  for (const [key, action] of Object.entries(actions)) {
-    finalActions[key] = (...args) => action(value$, ...args);
-  }
-  return [readonly(value$), finalActions];
-};
+//   useWatchEffect(() => {
+//     const value = typeof ref === "function" ? ref() : unref(ref);
+//     if (!done.current) {
+//       done.current = true;
+//       firstValue.current = value;
+//     } else {
+//       firstValue.current = null;
+//       setState(value);
+//     }
+//   }, options);
+//   const [state, setState] = useState(firstValue.current);
+//   return state;
+// };
 
-const watch = (fn, options) => {
-  let scheduler;
-  if (options.sync) {
-    scheduler = null;
-  } else {
-    ({ scheduler = nextTickScheduler } = options);
-  }
+// const justTrue = () => true;
 
-  return effect(fn, {
-    ...options,
-    scheduler,
-  });
-};
+// const Reactive = memo(({
+//   render,
+//   options,
+// }) => {
+//   return useWatchValue(render, options);
+// }, justTrue);
 
-const useWatchEffect = (fn, options = emptyObject) => {
-  const isDone = useRef();
-  const effectRef = useRef();
-
-  if (!isDone.current) {
-    isDone.current = true;
-    effectRef.current = watch(fn, options);
-  }
-
-  useEffect(() => {
-    if (!effectRef.current) {
-      effectRef.current = watch(fn, options);
-    }
-
-    return () => {
-      stop(effectRef.current);
-      effectRef.current = null;
-    };
-  }, [fn, options]);
-};
-
-const useWatch = (ref, options) => {
-  const isDone = useRef();
-  const firstValue = useRef();
-
-  const ef = useCallback(() => {
-    const value = typeof ref === "function" ? ref() : unref(ref);
-    if (!isDone.current) {
-      isDone.current = true;
-      firstValue.current = value;
-    } else {
-      firstValue.current = null;
-      setState(value);
-    }
-  }, [ref]);
-
-  useWatchEffect(ef, options);
-  const [state, setState] = useState(firstValue.current);
-  return state;
-};
-
-const useRunOnce = (factory) => {
-  const isDone = useRef();
-  const result = useRef();
-  if (!isDone.current) {
-    isDone.current = true;
-    pauseTracking();
-    result.current = factory();
-    resetTracking();
-  }
-
-  return result.current;
-};
-
-const Watch = ({
-  children,
-  sync,
-  scheduler,
-  onTrack,
-  onTrigger,
-  onStop,
-  allowRecurse,
-}) => {
-  const options = useMemo(
-    () => ({
-      sync,
-      scheduler,
-      onTrack,
-      onTrigger,
-      onStop,
-      allowRecurse,
-    }),
-    [sync, scheduler, onTrack, onTrigger, onStop, allowRecurse]
-  );
-  return useWatch(children, options);
-};
-
-const useToRef = (value) => {
-  const isDone = useRef();
-  const value$ = useRunOnce(() => {
-    return ref(value);
-  });
-
-  useEffect(() => {
-    if (!isDone.current) {
-      isDone.current = true;
-    } else {
-      value$.value = value;
-    }
-  }, [value$, value]);
-
-  return useRunOnce(() => readonly(value$));
-};
-
-const useReactiveProps = (props) => {
-  const isDone = useRef();
-  const props$ = useRunOnce(() => {
-    // react props is readonly
-    const _props = {};
-    for (const [key, value] of Object.entries(props)) {
-      _props[key] = isRef(value) ? value : ref(value);
-    }
-    return _props;
-  });
-
-  useEffect(() => {
-    if (!isDone.current) {
-      isDone.current = true;
-    } else {
-      for (const [key, value] of Object.entries(props)) {
-        if (!isRef(value)) {
-          props$[key].value = value;
+class Reactive extends Component {
+  constructor(props) {
+    super(props);
+    let done;
+    let firstValue;
+    this.runEffect = () => {
+      return effect(() => {
+        const { render } = this.props;
+        const value = typeof render === "function" ? render() : unref(render);
+        if (!done) {
+          done = true;
+          firstValue = value;
+        } else {
+          firstValue = null;
+          this.setState({ element: value });
         }
-      }
+      }, this.props.options);
     }
-  }, [props, props$]);
 
-  return props$;
-};
+    this.effectRef = this.runEffect();
 
-const propsToRefModels = (ReactiveComponent) => {
-  const NewComponent = (props) => {
-    const reactiveProps = useReactiveProps(props);
-    return useRunOnce(() => createElement(ReactiveComponent, reactiveProps));
-  };
+    this.state = {
+      element: firstValue
+    };
+  }
+  render() {
+    return this.state.element;
+  }
+  componentDidMount() {
+    if (!this.effectRef) {
+      this.effectRef = this.runEffect();
+    }
+  }
+  componentWillUnmount() {
+    stop(this.effectRef);
+    this.effectRef = null;
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.element !== this.state.element;
+  }
+}
 
-  NewComponent.displayName = `PropsToRefModels(${
-    ReactiveComponent.displayName || ReactiveComponent.name
-  })`;
-  return NewComponent;
-};
+const reactive = (render, options) => {
+  return createElement(Reactive, {
+    render,
+    options,
+  });
+}
+
+const component = new Proxy(new Map(), {
+  get(target, tagName) {
+    let Component = target.get(tagName);
+    if (!Component) {
+      Component = forwardRef(({ props, ...restProps}, ref) => {
+        return createElement(Reactive, {
+          render() {
+            return createElement(
+              tagName === 'Fragment' ? Fragment : tagName.toLowerCase(),
+              Object.assign(
+                {},
+                restProps,
+                typeof restProps.children === 'function' && { children: restProps.children() },
+                props?.(),
+                ref && { ref },
+              ),
+            )
+          },
+        });
+      });
+      Component.displayName = `Reactive.${tagName}`;
+      target.set(tagName, Component);
+    }
+    return Component;
+  },
+});
 
 export {
   setIsStaticRendering,
-  useWatchEffect,
-  useWatch,
-  Watch,
-  useRunOnce,
-  useToRef,
-  propsToRefModels,
-  refModel,
-  reactiveModel,
+  // useWatchEffect,
+  Reactive,
+  reactive,
+  component,
 };
