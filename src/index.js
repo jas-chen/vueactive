fe2b8972bbe54a3e83cd040e1ef6e53cc67c86e9
Jas Chen <jas@shoplineapp.com>
2021-09-04 22:45:19 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,300 +1,319 @@
-import React, { useMemo } from "react";
-import { toRef, ref } from "@vue/runtime-core";
+import React from "react";
+import { pauseTracking, resetTracking } from "@vue/reactivity";
 import {
-  component,
-  renderList,
-  setup,
-  computed,
-  useData,
-  createComponent,
-} from "vueactive";
+  reactive,
+  unref,
+  watch,
+  watchEffect,
+  isRef,
+  computed as _computed,
+} from "@vue/runtime-core";
 
-const { Input, Ul, Label, A, Li, Fragment, Strong } = component;
-
-const NewTodoInput = ({ onSubmit }) => {
-  return (
-    <input
-      ref={(element) => element?.focus()}
-      className="new-todo"
-      placeholder="What needs to be done?"
-      onKeyPress={(e) => {
-        if (e.key === "Enter" && e.target.value) {
-          onSubmit(e.target.value.trim());
-          e.target.value = "";
-        }
-      }}
-    />
-  );
-};
-
-const EditTodoInput = ({ initLabel, onSubmit, onFinish }) => {
-  return (
-    <input
-      className="edit"
-      ref={(input) => {
-        if (input) {
-          input.focus();
-        }
-      }}
-      defaultValue={initLabel}
-      onBlur={onFinish}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") {
-          onSubmit(e.target.value);
-          onFinish();
-        } else if (e.key === "Escape") {
-          vm.label = "";
-          onFinish();
-        }
-      }}
-    />
-  );
-};
-
-const TodoItem = createComponent({
-  displayName: "TodoItem",
-  setup: (props) => ({
-    computed: {
-      listClassName() {
-        return [
-          props.todo.done ? "completed" : "",
-          props.editingTodoId === props.todo.id ? "editing" : "",
-        ].join(" ");
-      },
-      label() {
-        return props.todo.label;
-      },
-      checked() {
-        return props.todo.done;
-      },
-    },
-    render: (vm) => (
-      <Li className={vm.listClassName}>
-        <div className="view">
-          <Input
-            type="checkbox"
-            className="toggle"
-            checked={vm.checked}
-            onChange={() => {
-              props.todo.done = !props.todo.done;
-            }}
-          />
-          <Label
-            onDoubleClick={() => {
-              props.editingTodoId = props.todo.id;
-            }}
-          >
-            {vm.label}
-          </Label>
-          <button className="destroy" onClick={props.onDestroyClick} />
-        </div>
-        <Fragment>
-          {computed(
-            () =>
-              props.editingTodoId === props.todo.id && (
-                <EditTodoInput
-                  initLabel={props.todo.label}
-                  onSubmit={(label) => {
-                    props.todo.label = label;
-                  }}
-                  onFinish={() => {
-                    props.editingTodoId = null;
-                  }}
-                />
-              )
-          )}
-        </Fragment>
-      </Li>
-    ),
-  }),
-});
-
-const FILTER_KEY_LABEL_MAP = {
-  ALL: "All",
-  ACTIVE: "Active",
-  COMPLETED: "Completed",
-};
-
-const FilterRow = createComponent({
-  displayName: "FilterRow",
-  setup: (props) => ({
-    computed: {
-      className() {
-        return props.routeName === props.filterKey ? "selected" : "";
-      },
-    },
-    render: (vm) => (
-      <li>
-        <A href={Router.getPath(props.filterKey)} className={vm.className}>
-          {FILTER_KEY_LABEL_MAP[props.filterKey]}
-        </A>
-      </li>
-    ),
-  }),
-});
-
-const App = () => {
-  const data = useData(() => ({
-    todoList: [],
-    editingTodoId: null,
-  }));
-
-  const vm = useMemo(() => {
-    return setup({
-      refs: {
-        routeName: Router.routeName$,
-        editingTodoId: toRef(data, "editingTodoId"),
-      },
-      computed: {
-        filteredTodoList() {
-          if (vm.routeName === "ALL") {
-            return data.todoList;
-          }
-
-          const filterValue = vm.routeName === "COMPLETED";
-          return data.todoList.filter(({ done }) => done === filterValue);
-        },
-        itemsLeft() {
-          return data.todoList.reduce(
-            (sum, todo) => (sum += todo.done ? 0 : 1),
-            0
-          );
-        },
-        isAllCompleted() {
-          return (
-            data.todoList.length > 0 && data.todoList.every((todo) => todo.done)
-          );
-        },
-      },
-      methods: {
-        addTodo(label) {
-          data.todoList.unshift({
-            id: new Date().getTime(),
-            label,
-            done: false,
-          });
-        },
-        deleteTodo(todo) {
-          data.todoList.splice(
-            data.todoList.findIndex(({ id }) => id === todo.id),
-            1
-          );
-        },
-        toggleAll() {
-          const done = !vm.isAllCompleted;
-          data.todoList.forEach((todo) => {
-            todo.done = done;
-          });
-        },
-        onClearCompletedClick() {
-          data.todoList = data.todoList.filter(({ done }) => !done);
-        },
-      },
-    });
-  }, []);
-
-  return useMemo(() => {
-    return (
-      <>
-        <div className="todoapp">
-          <header className="header">
-            <h1>todos</h1>
-            <NewTodoInput onSubmit={vm.addTodo} />
-          </header>
-          <section className="main">
-            <Input
-              id="toggle-all"
-              type="checkbox"
-              className="toggle-all"
-              checked={vm.isAllCompleted}
-              onChange={vm.toggleAll}
-            />
-            <label htmlFor="toggle-all" />
-            <Ul className="todo-list">
-              {renderList(vm.filteredTodoList, "id", (todo) => (
-                <TodoItem
-                  todo={todo}
-                  editingTodoId={vm.editingTodoId}
-                  onDestroyClick={() => {
-                    vm.deleteTodo(todo);
-                  }}
-                />
-              ))}
-            </Ul>
-          </section>
-          <footer className="footer">
-            <span className="todo-count">
-              <Strong>{vm.itemsLeft}</Strong> items left
-            </span>
-            <ul className="filters">
-              {renderList(
-                ["ALL", "ACTIVE", "COMPLETED"],
-                (filterKey) => filterKey,
-                (filterKey) => (
-                  <FilterRow filterKey={filterKey} routeName={vm.routeName} />
-                )
-              )}
-            </ul>
-            <button
-              className="clear-completed"
-              onClick={vm.onClearCompletedClick}
-            >
-              Clear completed
-            </button>
-          </footer>
-        </div>
-        <footer className="info">
-          <p>Double-click to edit a todo</p>
-          <p>
-            Created by <a href="http://github.com/jas-chen/">Jas Chen</a>
-          </p>
-          <p>
-            Part of <a href="http://todomvc.com">TodoMVC</a>
-          </p>
-        </footer>
-      </>
-    );
-  }, [vm]);
-};
-
-// ==== Router ====
-
-document.head.insertAdjacentHTML(
-  "beforeend",
-  '<link href="https://unpkg.com/todomvc-app-css@2.3.0/index.css" rel="stylesheet">'
-);
-
-const createRouter = (routerConfig, defaultUrl = "#/") => {
-  const paths = Object.keys(routerConfig);
-  const isPathValid = () => paths.includes(window.location.hash);
-
-  if (!isPathValid()) {
-    window.location.hash = defaultUrl;
+const getComponentName = (Component) => {
+  if (Component === React.Fragment) {
+    return "React.Fragment";
   }
 
-  const routeName$ = ref(routerConfig[window.location.hash]);
-
-  window.addEventListener("hashchange", () => {
-    if (!isPathValid()) {
-      window.location.hash = defaultUrl;
-    } else {
-      routeName$.value = routerConfig[window.location.hash];
-    }
-  });
-
-  return {
-    routeName$,
-    getPath: (routeName) =>
-      paths.find((hash) => routerConfig[hash] === routeName),
-  };
+  return typeof Component === "string"
+    ? Component
+    : Component.displayName || Component.name || "Anonymous";
 };
 
-const Router = createRouter({
-  "#/": "ALL",
-  "#/active": "ACTIVE",
-  "#/completed": "COMPLETED",
+const reactify = (tagName, options) => {
+  const sync = options?.sync || false;
+  const withRef = options?.withRef;
+
+  const Component = (props) => {
+    const { $$options, forwardedRef, ...restProps } = props;
+    const [, setState] = React.useState(0);
+    const reactiveProps = Object.keys(restProps)
+      .sort()
+      .map((key) => restProps[key])
+      .filter((value) => isRef(value));
+
+    React.useEffect(() => {
+      return watch(
+        reactiveProps,
+        () => {
+          setState((s) => s + 1);
+        },
+        { ...$$options, sync }
+      );
+    }, reactiveProps);
+
+    pauseTracking();
+    const childProps = Object.assign(
+      forwardedRef ? { ref: forwardedRef } : {},
+      mapValues(restProps, unref)
+    );
+    resetTracking();
+
+    return React.createElement(tagName, childProps);
+  };
+
+  const PropsGuard = (props) => {
+    const key = Object.entries(props)
+      .map(([key, value]) => {
+        if (isRef(value)) {
+          return `${key}$`;
+        }
+
+        return key;
+      })
+      .join("~");
+
+    return React.createElement(Component, { ...props, key });
+  };
+
+  Component.displayName = `reactive(${getComponentName(tagName)})`;
+  PropsGuard.displayName = `withReactiveProps(${Component.displayName})`;
+
+  if (withRef) {
+    return React.forwardRef(function ReactiveWithRef(props, ref) {
+      return React.createElement(PropsGuard, {
+        ...props,
+        forwardedRef: ref,
+      });
+    });
+  }
+
+  return PropsGuard;
+};
+
+const WITH_REF = "WithRef";
+
+const component = new Proxy(new Map(), {
+  get(target, tagName) {
+    let Component = target.get(tagName);
+    if (!Component) {
+      let withRef = false;
+      if (typeof tagName === "string" && tagName.endsWith(WITH_REF)) {
+        withRef = true;
+        tagName = tagName.replace(WITH_REF, "");
+      }
+
+      const finalTagName =
+        tagName === "Fragment" ? React.Fragment : tagName.toLowerCase();
+
+      Component = reactify(finalTagName, { withRef });
+      target.set(tagName, Component);
+    }
+    return Component;
+  },
 });
 
-// ==== Router ====
+const mapValues = (data, mapper) =>
+  Object.entries(data).reduce((result, [key, value]) => {
+    result[key] = mapper(value);
+    return result;
+  }, {});
 
-export default App;
+let autoUnRef = false;
+function computed(fn) {
+  const fnIsFunction = typeof fn === "function";
+  const finalFn = fnIsFunction ? fn : fn.get;
+  const config = {
+    get() {
+      autoUnRef = true;
+      try {
+        const result = finalFn.call(this);
+        autoUnRef = false;
+        return result;
+      } catch (e) {
+        autoUnRef = false;
+        throw e;
+      }
+    },
+  };
+  if (!fnIsFunction) {
+    config.set = fn.set;
+  }
+  return _computed(config);
+}
+
+function method(fn) {
+  const context = this;
+  return function () {
+    pauseTracking();
+    autoUnRef = true;
+    try {
+      const result = fn.apply(context, arguments);
+      resetTracking();
+      autoUnRef = false;
+      return result;
+    } catch (e) {
+      resetTracking();
+      autoUnRef = false;
+      throw e;
+    }
+  };
+}
+
+const setup = ({ computed: computedConfig, methods, refs }) => {
+  [computedConfig, methods, refs]
+    .map((obj) => (obj ? Object.keys(obj) : []))
+    .flat()
+    .forEach((key, _, keys) => {
+      if (keys.indexOf(key) !== keys.lastIndexOf(key)) {
+        throw new Error(`Key \`${key}\` is duplicated.`);
+      }
+    });
+
+  // spread to deal with react props
+  refs = refs && { ...refs };
+
+  const vm = new Proxy(
+    {},
+    {
+      get(target, key) {
+        for (const obj of seq) {
+          if (obj.hasOwnProperty(key)) {
+            const value = obj[key];
+            if (autoUnRef) {
+              return unref(value);
+            }
+            return value;
+          }
+        }
+        console.warn(`Key \`${key}\` not found.`);
+      },
+      set(target, key, value) {
+        for (const obj of seq) {
+          if (obj.hasOwnProperty(key)) {
+            obj[key].value = value;
+            return true;
+          }
+        }
+        console.warn(`Key \`${key}\` not found.`);
+        return true;
+      },
+    }
+  );
+
+  const computed$ =
+    computedConfig && mapValues(computedConfig, (fn) => computed.call(vm, fn));
+  const methods$ = methods && mapValues(methods, (fn) => method.call(vm, fn));
+  const seq = [computed$, methods$, refs].filter(Boolean);
+
+  return vm;
+};
+
+const createData = (data, props) => {
+  const props$ = props && setup({ refs: props });
+  pauseTracking();
+  autoUnRef = true;
+  let result;
+
+  try {
+    if (typeof data === "function") {
+      result = data(props$);
+    } else {
+      result = data;
+    }
+  } catch (e) {
+    autoUnRef = false;
+    resetTracking();
+    throw e;
+  }
+
+  autoUnRef = false;
+  resetTracking();
+  return reactive(result);
+};
+
+const useData = (data, props) => {
+  return React.useState(() => {
+    return createData(data, props);
+  })[0];
+};
+
+const withReactiveProps = (Component, displayName) => {
+  const PropsGuard = (props) => {
+    const key = Object.entries(props)
+      .map(([key, value]) => {
+        if (isRef(value)) {
+          return `${key}$`;
+        }
+
+        return key;
+      })
+      .join("~");
+
+    return React.createElement(Component, {
+      ...props,
+      key,
+      $$PROPS_GUARD: true,
+    });
+  };
+
+  if (displayName) {
+    Component.displayName = displayName;
+  }
+
+  PropsGuard.displayName = `withReactiveProps(${getComponentName(Component)})`;
+
+  return PropsGuard;
+};
+
+const useReactiveProps = (props, fn) => {
+  if (!props.hasOwnProperty("$$PROPS_GUARD")) {
+    throw new Error("useReactiveProps must use with withReactiveProps.");
+  }
+  const reactiveProps = Object.keys(props)
+    .sort()
+    .map((key) => props[key]);
+
+  return React.useMemo(() => {
+    const props$ = setup({
+      refs: props,
+    });
+
+    if (fn) {
+      return fn(props$);
+    }
+
+    return props$;
+  }, reactiveProps);
+};
+
+const useWatch = (fn, option, deps = []) => {
+  if (Array.isArray(option)) {
+    option = undefined;
+    deps = option;
+  }
+
+  React.useEffect(() => {
+    return watchEffect(() => {
+      autoUnRef = true;
+      try {
+        fn();
+      } catch (e) {}
+      autoUnRef = false;
+    }, option);
+  }, deps);
+};
+
+const createComponent = ({ displayName, setup: setupFn }) => {
+  const Component = withReactiveProps((props) => {
+    return useReactiveProps(props, (props) => {
+      const config = typeof setupFn === "function" ? setupFn(props) : setupFn;
+      const vm = setup(config);
+      return config.render.call(vm);
+    });
+  }, displayName);
+
+  return Component;
+};
+
+export {
+  reactify,
+  component,
+  setup,
+  method,
+  useData,
+  createData,
+  withReactiveProps,
+  useReactiveProps,
+  useWatch,
+  createComponent,
+};
